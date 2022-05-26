6983965598339c958fad04187389d6d1d8a7de80
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-05-26 10:20:10 +0800

    Bump version: 0.1.4 → 0.1.5

--- envchain_shellenv/__init__.py
+++ envchain_shellenv/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __app_name__ = 'envchain-shellenv'
 __description_long__ = (
     'envchain shellenv - prints export statements for your secrets in the keychain'
