8df9209f1099aea67149caef99cb9655251dc1d7
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-11-27 14:17:51 +0800

    Bump version: 1.0.1 → 1.0.2

--- envchain_shellenv/__init__.py
+++ envchain_shellenv/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __app_name__ = 'envchain-shellenv'
 __description_long__ = (
     'envchain shellenv - prints export statements for your secrets in the keychain'
